2ee941580ec7bb3d60aad92e5dd627c02a2b635e
mariafsoares <107869988+mariafsoares@users.noreply.github.com>
2022-06-20 15:07:03 -0300

    Update PE9_streamlit.py

--- PE9_streamlit.py
+++ PE9_streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 
-DATA_URL = ("C:/Users/mariso15/Documents/PE9_streamlit_data.xlsx")
+DATA_URL = ("./PE9_streamlit_data.xlsx")
 
 st.set_page_config(page_title="VOAs PE9",
                    layout="centered",
